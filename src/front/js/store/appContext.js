e44af7d9626b05ee4a48d292a8b05de4b6fa7b07
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-03-12 14:55:30 +0000

    Moved google login to flux

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -27,78 +27,11 @@ const injectContext = PassedComponent => {
 		const [ user, setUser ] = useState({});
 		
 		
-		const checkEmail = async (email, name) => {
-			try {
-				const response = await fetch(process.env.BACKEND_URL + "/api/users/" + email);
-				const data = await response.json();
-				
-				if (data.message == 'true') {
-					// Email exists, call the login function here
-					await handleGoogleLogin(email);
-					console.log("Email exists:", data);
-					
-				} else {
-					// Email does not exist, call the signup function here
-					console.log("Email does not exist:", data);
-					// Call the signup function here
-					// Then call the login function with just the email to create the token.
-					 await state.actions.signup({
-						first_name: name,
-						email: email,
-					});
-					await handleGoogleLogin(email);
-				
-					return data;}
-			} catch (error) {
-				console.error('Error checking if email exists:', error);
-			}
-		}
-		
-		const handleGoogleLogin = async (email) => {
-			try {
-			
-			  const response = await fetch(process.env.BACKEND_URL + "/api/GoogleLogin" , {
-				method: "POST",
-				headers: {
-				  "Content-Type": "application/json",
-				},
-				body: JSON.stringify({ email }),
-			  });
-			  
-			  if (!response.ok) {
-				throw new Error('Login failed. Please check your email and password.');
-			  }
-			  
-			  const data = await response.json();
-				  
-			  localStorage.setItem("accessToken", data.access_token)
-		  
-			  state.actions.setAccessToken(data.access_token);
-			  state.actions.setIsLoggedIn(true);
-			    
-		  
-			} catch (error) {
-			  console.error('Error during login:', error);
-			  alert(error.message); // Display alert for error message
-			}
-		  }
-		
-			
-
-		  //put in flux
-        async function handleCallbackResponse(response) {
-            console.log("Encoded JWT ID token: " + response.credential);
-            var userObject = jwtDecode(response.credential);
-            console.log(userObject);
-            //here we use the object returned to sign up or login the user.
-            checkEmail(userObject.email, userObject.name);
-        }
-		
 
 		useEffect(() => {
 			 google.accounts.id.initialize ({
 				client_id: "533568438503-75kgn3gkshmbrlnhsg2ithfchvc10ebi.apps.googleusercontent.com",
-				callback: handleCallbackResponse //state.actions.handle
+				callback: state.actions.handleCallbackResponse //state.actions.handle
 			});
 		
 			google.accounts.id.renderButton (
